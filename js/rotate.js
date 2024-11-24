ac93c4b199a25e838f2d82a3ac0e3946bc509ebe
vika-tsymbaliyk <125582375+vika-tsymbaliyk@users.noreply.github.com>
2024-11-24 15:48:02 +0200

    start

---
+++ js/rotate.js
@@ -0,0 +1,22 @@
+   function rotateDIV() {
+    
+   var rotated = false;
+
+    document.getElementById('construction').onmouseover = function() {
+        var div = document.getElementById('construction'),
+            deg = rotated ? 0 : 180;
+
+        div.style.mozTransform    = 'rotate('+deg+'deg)'; 
+        div.style.msTransform     = 'rotate('+deg+'deg)'; 
+        div.style.oTransform      = 'rotate('+deg+'deg)'; 
+        div.style.transform       = 'rotate('+deg+'deg)'; 
+        alert("ne touche pas mon suris")
+
+        // div.style.mozTransform    = 'scale(2)'; 
+        // div.style.msTransform     = 'scale(2)'; 
+        // div.style.oTransform      = 'scale(2)'; 
+        // div.style.transform       = 'scale(2)'; 
+
+        rotated = !rotated;
+    }
+}
